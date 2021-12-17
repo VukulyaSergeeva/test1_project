9a8a2cd7ef311a24c076cd62fcf9ba3cb7e4748e
Victoria Sergeeva <vikulya_sergeeva99@inbox.ruvikulya_sergeeva99@inbox.ruvikulya_sergeeva99@inbox.ruvikulya_sergeeva99@inbox.ruvikulya_sergeeva99@inbox.ru>
2021-12-17 20:27:53 +0300

    added new page and test

--- test1_project/TestClass.cs
+++ test1_project/TestClass.cs
@@ -1,10 +1,34 @@
 ﻿using System;
+using NUnit.Framework;
+using test1_project;
+
 namespace test1_project
 {
     public class TestClass
     {
-        public TestClass()
+        FormPage page;
+        [Test]
+        public void FirstTest()
         {
+            page = new FormPage("https://demoqa.com/automation-practice-form");
+            page.fillName("Tom", "Sterling");
+            page.fillEmail("jdgjksbgsj@jhdgfs.fe");
+            page.selectGender("Male");
+            page.uploadPicture("/Users/viktoriasergeeva/Desktop/фото мои/198.jpg");
+            page.fillAddress("Могилев,ул.Челюскинцев 170В,62");
+            page.fillPhone("1234567891");
+            page.fillSubjects("maths, physics");
+            page.selectCity("Karnal");
+            page.hobbies("Music","Sports");
+            page.clickSubmit();
+            Assert.IsTrue(page.CheckForm(), "Form Is Not Valid");
+
         }
+        [TearDown]
+        public void TearDown()
+        {
+            page.Quit();
+        }
+
     }
 }
